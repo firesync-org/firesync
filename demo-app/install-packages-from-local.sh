2196148378de8766cab510146982b723a2da2eff
James Allen <james@firesync.live>
2023-03-10 16:43:00 +0000

    Don't update package.json everytime local packages are updated

--- demo-app/install-packages-from-local.sh
+++ demo-app/install-packages-from-local.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
+
+# Don't update package.json with local versions - this is just for dev
+cp package.json package.json.bak
+cp package-lock.json package-lock.json.bak
+
 (
   cd ../firesync-client &&
   ([ ! -e firesync-client-*.tgz  ] || rm firesync-client-*.tgz) && 
@@ -15,3 +20,6 @@ npm install ../firesync-client/firesync-client-*.tgz
   npm pack .
 )
 npm install ../firesync-server/firesync-server-*.tgz
+
+mv package.json.bak package.json
+mv package-lock.json.bak package-lock.json
