b74955893c30a97140de242e42aad56eeac55634
James Allen <james@firesync.live>
2023-03-31 12:05:15 +0100

    add open telem / honeycomb to firesync-server

--- firesync-server/src/server/index.ts
+++ firesync-server/src/server/index.ts
@@ -1,3 +1,18 @@
+("use strict");
+
+const { HoneycombSDK } = require('@honeycombio/opentelemetry-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+
+// uses the HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
+const sdk = new HoneycombSDK({
+  instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start()
+
+
 import express from 'express'
 import expressLayouts from 'express-ejs-layouts'
 import path from 'path'
